06b770f10e7afb57e9635fb6c0a4bdeca66173b9
Braniel J. Pichardo <branieljose@gmail.com>
2016-12-09 00:17:48 -0500

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+var questions = [
+  {
+  question: 'What was the average life expectancy of white males born in the U.S. just before the Civil War?' ,
+  choices: ['40 years','50 years','60 years','70 years'] ,
+  answer: '40 years'
+  }
+];
